27f5b41dda7522e60a12e4edcc8a74137d2a5b25
khanh <huy.khanh.ha@uni-weimar.de>
2019-09-15 17:53:22 +0200

    connect mongodb server

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const { promisify } = require('util');
 const writeFile = promisify(require('fs').writeFile);
 const { Pipe } = require('./lib');
+const MongoClient = require('mongodb').MongoClient;
+
+const mongoUrl = 'mongodb://localhost:27017';
+const dbName = 'brands-crawl';
 
 (async () => {
+    const mgClient = new MongoClient(mongoUrl);
+    try{
+        await mgClient.connect();
+        console.log('Connected to MongoDB sucessfully');
+        const db = client.db(dbName);
+    }catch(err){
+        console.log('Failed to connect to Mongo client');
+    }
+
     const startDate = new Date().getTime();
     Pipe.Pipeline(
         require('./ninomax/init'),
